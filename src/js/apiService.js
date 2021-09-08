3945c720e2497b037e6698f6ec790fd9b5ed8889
natashagoryanina <natashagoryanina@gmail.com>
2021-09-08 23:10:42 +0300

    gallery

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,14 +1,26 @@
 export default class PhotosApiService {
     constructor() {
         this.searchPhoto = '';
+        this.page = 1;
     }
 
     fetchElementByName() {
         const API_KEY = '23294287-f28ad0716a186f6195177ba0b';
         
-        fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.searchPhoto}&page=1&per_page=12&key=${API_KEY}`)
-        .then(r => r.json())
-        .then(console.log);
+        return fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.searchPhoto}&page=${this.page}&per_page=12&key=${API_KEY}`)
+        .then(response => response.json())
+        .then(({hits}) => {
+            this.incrementPage();
+            return hits;
+        });
+    }
+
+    incrementPage() {
+        this.page += 1;
+    }
+
+    resetPage() {
+        this.page = 1;
     }
 
     get query() {
--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,51 @@
 import PhotosApiService from './apiService';
+import imagesTmpl from '../templates/image-card.hbs';
+import { error } from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+
 const refs = {
     searchForm: document.querySelector('.search-form'),
+    loadMoreBtn: document.querySelector('.btn'),
+    gallery: document.querySelector('.gallery'),
+    galleryItem: document.querySelector('.gallery-item'),
 }
 
 const photosApiService = new PhotosApiService();
 
 refs.searchForm.addEventListener('submit', onFormSubmit);
+refs.loadMoreBtn.addEventListener('click', onBtnClick);
 
 function onFormSubmit(e) {
     e.preventDefault();
 
+    clearImgContainer();
     photosApiService.query = e.currentTarget.elements.query.value;
-    photosApiService.fetchElementByName();
+
+    if (photosApiService.query === '') {
+        return error({ text: 'Please, make a request.'});
+    };
+
+    refs.loadMoreBtn.removeAttribute('disabled');
+    photosApiService.resetPage();
+    photosApiService.fetchElementByName()
+        .then(imagesMarkup)
+        .catch(error => error({ text: '0 results' }))
 }
+
+function onBtnClick(e) {
+    photosApiService.fetchElementByName().then(imagesMarkup);
+}
+
+function imagesMarkup(hits) {
+    refs.gallery.insertAdjacentHTML('beforeend', imagesTmpl(hits));
+}
+
+function clearImgContainer() {
+    refs.gallery.innerHTML = '';
+}
+
+refs.gallery.scrollIntoView({
+  behavior: 'smooth',
+  block: 'end',
+});
